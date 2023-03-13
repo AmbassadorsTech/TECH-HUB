9709cd38508ad72a663af2d144ea86595a0cf071
paul <pauleric363@gmail.com>
2023-03-13 04:44:33 +0100

    First Commit

---
+++ JAVA SCRIPT/dom.js
@@ -0,0 +1,14 @@
+// DOM MANIPULATION
+
+//GetElementById
+const tittle = document.getElementById('main-heading');
+console.log(tittle);
+
+//getElementByClassName
+
+const listItem = document.getElementsByClassName('list-items');
+console.log(listItem);
+
+const name = document.querySelector('main-heading');
+name.style.color = 'red';
+console.log(name);
---
+++ JAVA SCRIPT/first.js
@@ -0,0 +1,47 @@
+console.log("paul eric")
+let name ="ERIC PAUL"
+console.log(name);
+
+// FUNCTION
+
+function welcome(){
+    return true
+}
+console.log(welcome);
+
+function minusSeven(num) {
+    return num - 7
+}
+console.log(minusSeven(20));
+
+//OBJECT {}
+
+let studentDetails ={
+    name: "PAUL",
+    age: 30,
+    level: 300,
+    department: "computer Science",
+}
+console.log(studentDetails);
+console.log(studentDetails.name);
+
+//ARRAY [] data structure used to represent a list of items
+let family =['father', 'mother', 'children']
+console.log(family);
+console.log(family[0]);
+//to add to an array
+family[3]='uncle'
+console.log(family); 
+
+//FUNCTION is a statement that performs a task or calculate a statement
+
+function greet(name) { // name is the parameter of the greet function
+    console.log('hello ' + name)
+    
+}
+greet('Paul'); // Paul is the argument of the greet function
+
+
+
+
+
